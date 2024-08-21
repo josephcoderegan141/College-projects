e63bca96a6a26ea025747285df8fa405d5d89bca
josephcoderegan141 <160789417+josephcoderegan141@users.noreply.github.com>
2024-08-21 19:08:19 +0100

    Add files via upload

---
+++ Base conversion/base.java
@@ -0,0 +1,31 @@
+
+package try1;
+
+import java.util.Scanner;
+
+public class base {
+	
+	public static void main(String[] args) {
+	
+	Scanner scan = new Scanner(System.in);
+	
+
+	String s1 = scan.nextLine(); // number N
+	int x = scan.nextInt(); // base that n is currently in
+	int y = scan.nextInt(); // base we want N in 
+	}
+}
+	
+	
+//	if(x<2||x>32) {
+//		System.out.print("ofb");
+//	}
+//	
+//	if(y<2||y>32) {
+//		System.out.print("ofb");
+//	}
+	
+
+
+
+
